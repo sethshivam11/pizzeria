0ac253b1ce115bd0073a47fe1b7084bdfdbdafd2
Shivam Soni <130232913+sethshivam11@users.noreply.github.com>
2026-08-14 07:13:46 +0000

    Added exit link

--- pizzeriaapp/src/components/Footer.jsx
+++ pizzeriaapp/src/components/Footer.jsx
@@ -1,7 +1,20 @@
 function Footer() {
   return (
-    <footer className="py-2 text-center border-top" style={{ color: "gold" }}>
-      Copyrights &copy; 2017 Pizzeria. All rights reserved
+    <footer
+      className="d-flex flex-column-reverse flex-sm-row align-items-center justify-content-between p-2 text-center border-top"
+      style={{ color: "gold" }}
+    >
+      <span>Copyrights &copy; 2017 Pizzeria. All rights reserved</span>
+      <span>
+        Developed by{" "}
+        <a
+          href="https://dev-shivam.in"
+          className="link-underline-warning link-underline-opacity-0 link-underline-opacity-75-hover"
+          style={{ color: "gold" }}
+        >
+          Shivam
+        </a>
+      </span>
     </footer>
   );
 }
